2b51f6e53491f4de89d5fc774f78877286e5f924
Igloo Lik <igloolik@proton.me>
2025-05-29 14:21:05 +1000

    initial setup

---
+++ program.cpp
@@ -0,0 +1,19 @@
+#include "splashkit.h"
+
+int main()
+{
+    // open a window and clear it to white
+    open_window("My Interface!", 800, 600);
+    clear_screen(COLOR_WHITE);
+
+    // ...we'll put our interface code here!...
+
+    // refresh the screen, then wait 5 seconds
+    refresh_screen();
+    delay(5000);
+
+    // close all open windows
+    close_all_windows();
+
+    return 0;
+}
