938cba5411f434158efe7977f94bdd7a6c39e6fa
Igloo Lik <igloolik@proton.me>
2025-05-29 15:07:49 +1000

    newest stuff

--- program.cpp
+++ program.cpp
@@ -1,16 +1,31 @@
 #include "splashkit.h"
 
+const int PROG_HEIGHT = 800;
+const int PROG_WIDTH  = 450;
+
 int main()
 {
     // open a window and clear it to white
-    open_window("My Interface!", 800, 600);
-    clear_screen(COLOR_WHITE);
+    open_window("My Interface!", PROG_WIDTH, PROG_HEIGHT);
+    clear_screen(COLOR_DARK_GRAY);
 
-    // ...we'll put our interface code here!...
+    while (!window_close_requested("My Interface!"))
+    {
+        // process user events
+        process_events();
 
-    // refresh the screen, then wait 5 seconds
-    refresh_screen();
-    delay(5000);
+        // program here
+
+
+         // show button
+        button("My Button!", rectangle_from(PROG_WIDTH / 2, PROG_HEIGHT / 2, PROG_WIDTH / 4, 24));
+
+        // draw the interface!
+        draw_interface();
+
+         // refresh the screen
+        refresh_screen();
+    }
 
     // close all open windows
     close_all_windows();
